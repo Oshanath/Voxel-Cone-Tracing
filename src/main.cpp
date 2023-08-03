e6e0cfd8db17c7a05c71cc3f5bedab3f6e896eb4
Oshanath Rajawasam <oshanath.19@cse.mrt.ac.lk>
2023-08-03 17:01:31 +0530

    Vulkan works

--- src/main.cpp
+++ src/main.cpp
@@ -2,62 +2,10 @@
 #include <camera.h>
 #include <material.h>
 #include <mesh.h>
-#include <ogl.h>
+#include <vk.h>
 #include <profiler.h>
 #include <assimp/scene.h>
-
-// Embedded vertex shader source.
-const char* g_sample_vs_src = R"(
-layout (location = 0) in vec4 VS_IN_Position;
-layout (location = 1) in vec4 VS_IN_TexCoord;
-layout (location = 2) in vec4 VS_IN_Normal;
-layout (location = 3) in vec4 VS_IN_Tangent;
-layout (location = 4) in vec4 VS_IN_Bitangent;
-layout (std140) uniform Transforms //#binding 0
-{ 
-	mat4 model;
-	mat4 view;
-	mat4 projection;
-};
-out vec3 PS_IN_FragPos;
-out vec3 PS_IN_Normal;
-out vec2 PS_IN_TexCoord;
-void main()
-{
-    vec4 position = model * vec4(VS_IN_Position.xyz, 1.0);
-	PS_IN_FragPos = position.xyz;
-	PS_IN_Normal = mat3(model) * VS_IN_Normal.xyz;
-	PS_IN_TexCoord = VS_IN_TexCoord.xy;
-    gl_Position = projection * view * position;
-}
-)";
-
-// Embedded fragment shader source.
-const char* g_sample_fs_src = R"(
-precision mediump float;
-out vec4 PS_OUT_Color;
-in vec3 PS_IN_FragPos;
-in vec3 PS_IN_Normal;
-in vec2 PS_IN_TexCoord;
-uniform sampler2D s_Diffuse; //#slot 0
-void main()
-{
-	vec3 light_pos = vec3(-200.0, 200.0, 0.0);
-	vec3 n = normalize(PS_IN_Normal);
-	vec3 l = normalize(light_pos - PS_IN_FragPos);
-	float lambert = max(0.0f, dot(n, l));
-    vec3 diffuse = texture(s_Diffuse, PS_IN_TexCoord).xyz;// + vec3(1.0);
-	vec3 ambient = diffuse * 0.03;
-	vec3 color = diffuse * lambert + ambient;
-
-    // HDR tonemapping
-    color = color / (color + vec3(1.0));
-    // gamma correct
-    color = pow(color, vec3(1.0 / 2.2));
-
-    PS_OUT_Color = vec4(color, 1.0);
-}
-)";
+#include <vk_mem_alloc.h>
 
 // Uniform buffer data structure.
 struct Transforms
@@ -77,9 +25,6 @@ protected:
 
     bool init(int argc, const char* argv[]) override
     {
-        // Set initial GPU states.
-        set_initial_states();
-
         // Create GPU resources.
         if (!create_shaders())
             return false;
@@ -91,6 +36,11 @@ protected:
         if (!load_mesh())
             return false;
 
+        create_descriptor_set_layout();
+        create_descriptor_set();
+        write_descriptor_set();
+        create_pipeline_state();
+
         // Create camera.
         create_camera();
 
@@ -101,27 +51,46 @@ protected:
 
     void update(double delta) override
     {
-        DW_SCOPED_SAMPLE("update");
+        dw::vk::CommandBuffer::Ptr cmd_buf = m_vk_backend->allocate_graphics_command_buffer();
 
-        // Render profiler.
-        dw::profiler::ui();
+        VkCommandBufferBeginInfo begin_info;
+        DW_ZERO_MEMORY(begin_info);
 
-        // Update camera.
-        m_main_camera->update();
+        begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
-        // Update uniforms.
-        update_uniforms();
+        vkBeginCommandBuffer(cmd_buf->handle(), &begin_info);
 
-        // Render.
-        render();
+        {
+            DW_SCOPED_SAMPLE("update", cmd_buf);
+
+            // Render profiler.
+            dw::profiler::ui();
+
+            // Update camera.
+            m_main_camera->update();
+
+            // Update uniforms.
+            update_uniforms(cmd_buf);
+
+            // Render.
+            render(cmd_buf);
+        }
+
+        vkEndCommandBuffer(cmd_buf->handle());
+
+        submit_and_present({ cmd_buf });
     }
 
     // -----------------------------------------------------------------------------------------------------------------------------------
 
     void shutdown() override
     {
-        // Unload assets.
         m_mesh.reset();
+        m_pso.reset();
+        m_pipeline_layout.reset();
+        m_per_frame_ds_layout.reset();
+        m_per_frame_ds.reset();
+        m_ubo.reset();
     }
 
     // -----------------------------------------------------------------------------------------------------------------------------------
@@ -131,9 +100,10 @@ protected:
         // Set custom settings here...
         dw::AppSettings settings;
 
-        settings.width  = 1280;
-        settings.height = 720;
-        settings.title  = "Hello dwSampleFramework (OpenGL)";
+        settings.width       = 1280;
+        settings.height      = 720;
+        settings.title       = "Hello dwSampleFramework (Vulkan)";
+        settings.ray_tracing = false;
 
         return settings;
     }
@@ -153,53 +123,197 @@ private:
 
     bool create_shaders()
     {
-        // Create shaders
-        m_vs = dw::gl::Shader::create(GL_VERTEX_SHADER, g_sample_vs_src);
-        m_fs = dw::gl::Shader::create(GL_FRAGMENT_SHADER, g_sample_fs_src);
-
-        if (!m_vs || !m_fs)
-        {
-            DW_LOG_FATAL("Failed to create Shaders");
-            return false;
-        }
-
-        // Create shader program
-        m_program = dw::gl::Program::create({ m_vs, m_fs });
-
-        if (!m_program)
-        {
-            DW_LOG_FATAL("Failed to create Shader Program");
-            return false;
-        }
-
-        m_program->uniform_block_binding("Transforms", 0);
-
         return true;
-    }
-
-    // -----------------------------------------------------------------------------------------------------------------------------------
-
-    void set_initial_states()
-    {
-        glEnable(GL_DEPTH_TEST);
-        glCullFace(GL_BACK);
     }
 
     // -----------------------------------------------------------------------------------------------------------------------------------
 
     bool create_uniform_buffer()
     {
-        // Create uniform buffer for matrix data
-        m_ubo = dw::gl::Buffer::create(GL_UNIFORM_BUFFER, GL_MAP_WRITE_BIT, sizeof(Transforms), nullptr);
+        m_ubo_size = m_vk_backend->aligned_dynamic_ubo_size(sizeof(Transforms));
+        m_ubo      = dw::vk::Buffer::create(m_vk_backend, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, m_ubo_size * dw::vk::Backend::kMaxFramesInFlight, VMA_MEMORY_USAGE_CPU_TO_GPU, VMA_ALLOCATION_CREATE_MAPPED_BIT);
 
         return true;
     }
 
     // -----------------------------------------------------------------------------------------------------------------------------------
 
+    void create_descriptor_set_layout()
+    {
+        dw::vk::DescriptorSetLayout::Desc desc;
+
+        desc.add_binding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 1, VK_SHADER_STAGE_VERTEX_BIT);
+
+        m_per_frame_ds_layout = dw::vk::DescriptorSetLayout::create(m_vk_backend, desc);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------------------------
+
+    void create_descriptor_set()
+    {
+        m_per_frame_ds = m_vk_backend->allocate_descriptor_set(m_per_frame_ds_layout);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------------------------
+
+    void write_descriptor_set()
+    {
+        VkDescriptorBufferInfo buffer_info;
+
+        buffer_info.buffer = m_ubo->handle();
+        buffer_info.offset = 0;
+        buffer_info.range  = sizeof(Transforms);
+
+        VkWriteDescriptorSet write_data;
+        DW_ZERO_MEMORY(write_data);
+
+        write_data.sType           = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+        write_data.descriptorCount = 1;
+        write_data.descriptorType  = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC;
+        write_data.pBufferInfo     = &buffer_info;
+        write_data.dstBinding      = 0;
+        write_data.dstSet          = m_per_frame_ds->handle();
+
+        vkUpdateDescriptorSets(m_vk_backend->device(), 1, &write_data, 0, nullptr);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------------------------
+
+    void create_pipeline_state()
+    {
+        // ---------------------------------------------------------------------------
+        // Create shader modules
+        // ---------------------------------------------------------------------------
+
+        dw::vk::ShaderModule::Ptr vs = dw::vk::ShaderModule::create_from_file(m_vk_backend, "shaders/mesh.vert.spv");
+        dw::vk::ShaderModule::Ptr fs = dw::vk::ShaderModule::create_from_file(m_vk_backend, "shaders/mesh.frag.spv");
+
+        dw::vk::GraphicsPipeline::Desc pso_desc;
+
+        pso_desc.add_shader_stage(VK_SHADER_STAGE_VERTEX_BIT, vs, "main")
+            .add_shader_stage(VK_SHADER_STAGE_FRAGMENT_BIT, fs, "main");
+
+        // ---------------------------------------------------------------------------
+        // Create vertex input state
+        // ---------------------------------------------------------------------------
+
+        pso_desc.set_vertex_input_state(m_mesh->vertex_input_state_desc());
+
+        // ---------------------------------------------------------------------------
+        // Create pipeline input assembly state
+        // ---------------------------------------------------------------------------
+
+        dw::vk::InputAssemblyStateDesc input_assembly_state_desc;
+
+        input_assembly_state_desc.set_primitive_restart_enable(false)
+            .set_topology(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST);
+
+        pso_desc.set_input_assembly_state(input_assembly_state_desc);
+
+        // ---------------------------------------------------------------------------
+        // Create viewport state
+        // ---------------------------------------------------------------------------
+
+        dw::vk::ViewportStateDesc vp_desc;
+
+        vp_desc.add_viewport(0.0f, 0.0f, m_width, m_height, 0.0f, 1.0f)
+            .add_scissor(0, 0, m_width, m_height);
+
+        pso_desc.set_viewport_state(vp_desc);
+
+        // ---------------------------------------------------------------------------
+        // Create rasterization state
+        // ---------------------------------------------------------------------------
+
+        dw::vk::RasterizationStateDesc rs_state;
+
+        rs_state.set_depth_clamp(VK_FALSE)
+            .set_rasterizer_discard_enable(VK_FALSE)
+            .set_polygon_mode(VK_POLYGON_MODE_FILL)
+            .set_line_width(1.0f)
+            .set_cull_mode(VK_CULL_MODE_BACK_BIT)
+            .set_front_face(VK_FRONT_FACE_COUNTER_CLOCKWISE)
+            .set_depth_bias(VK_FALSE);
+
+        pso_desc.set_rasterization_state(rs_state);
+
+        // ---------------------------------------------------------------------------
+        // Create multisample state
+        // ---------------------------------------------------------------------------
+
+        dw::vk::MultisampleStateDesc ms_state;
+
+        ms_state.set_sample_shading_enable(VK_FALSE)
+            .set_rasterization_samples(VK_SAMPLE_COUNT_1_BIT);
+
+        pso_desc.set_multisample_state(ms_state);
+
+        // ---------------------------------------------------------------------------
+        // Create depth stencil state
+        // ---------------------------------------------------------------------------
+
+        dw::vk::DepthStencilStateDesc ds_state;
+
+        ds_state.set_depth_test_enable(VK_TRUE)
+            .set_depth_write_enable(VK_TRUE)
+            .set_depth_compare_op(VK_COMPARE_OP_LESS)
+            .set_depth_bounds_test_enable(VK_FALSE)
+            .set_stencil_test_enable(VK_FALSE);
+
+        pso_desc.set_depth_stencil_state(ds_state);
+
+        // ---------------------------------------------------------------------------
+        // Create color blend state
+        // ---------------------------------------------------------------------------
+
+        dw::vk::ColorBlendAttachmentStateDesc blend_att_desc;
+
+        blend_att_desc.set_color_write_mask(VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT)
+            .set_blend_enable(VK_FALSE);
+
+        dw::vk::ColorBlendStateDesc blend_state;
+
+        blend_state.set_logic_op_enable(VK_FALSE)
+            .set_logic_op(VK_LOGIC_OP_COPY)
+            .set_blend_constants(0.0f, 0.0f, 0.0f, 0.0f)
+            .add_attachment(blend_att_desc);
+
+        pso_desc.set_color_blend_state(blend_state);
+
+        // ---------------------------------------------------------------------------
+        // Create pipeline layout
+        // ---------------------------------------------------------------------------
+
+        dw::vk::PipelineLayout::Desc pl_desc;
+
+        pl_desc.add_descriptor_set_layout(m_per_frame_ds_layout)
+            .add_descriptor_set_layout(dw::Material::descriptor_set_layout());
+
+        m_pipeline_layout = dw::vk::PipelineLayout::create(m_vk_backend, pl_desc);
+
+        pso_desc.set_pipeline_layout(m_pipeline_layout);
+
+        // ---------------------------------------------------------------------------
+        // Create dynamic state
+        // ---------------------------------------------------------------------------
+
+        pso_desc.add_dynamic_state(VK_DYNAMIC_STATE_VIEWPORT)
+            .add_dynamic_state(VK_DYNAMIC_STATE_SCISSOR);
+
+        // ---------------------------------------------------------------------------
+        // Create pipeline
+        // ---------------------------------------------------------------------------
+
+        pso_desc.set_render_pass(m_vk_backend->swapchain_render_pass());
+
+        m_pso = dw::vk::GraphicsPipeline::create(m_vk_backend, pso_desc);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------------------------
+
     bool load_mesh()
     {
-        m_mesh = dw::Mesh::load("teapot.obj");
+        m_mesh = dw::Mesh::load(m_vk_backend, "teapot.obj");
         return m_mesh != nullptr;
     }
 
@@ -213,29 +327,62 @@ private:
 
     // -----------------------------------------------------------------------------------------------------------------------------------
 
-    void render()
+    void render(dw::vk::CommandBuffer::Ptr cmd_buf)
     {
-        DW_SCOPED_SAMPLE("render");
+        DW_SCOPED_SAMPLE("render", cmd_buf);
 
-        // Bind framebuffer and set viewport.
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-        glViewport(0, 0, m_width, m_height);
+        VkClearValue clear_values[2];
 
-        // Clear default framebuffer.
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        clear_values[0].color.float32[0] = 0.0f;
+        clear_values[0].color.float32[1] = 0.0f;
+        clear_values[0].color.float32[2] = 0.0f;
+        clear_values[0].color.float32[3] = 1.0f;
 
-        // Bind shader program.
-        m_program->use();
+        clear_values[1].color.float32[0] = 1.0f;
+        clear_values[1].color.float32[1] = 1.0f;
+        clear_values[1].color.float32[2] = 1.0f;
+        clear_values[1].color.float32[3] = 1.0f;
 
-        // Bind uniform buffer.
-        m_ubo->bind_base(0);
+        VkRenderPassBeginInfo info    = {};
+        info.sType                    = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+        info.renderPass               = m_vk_backend->swapchain_render_pass()->handle();
+        info.framebuffer              = m_vk_backend->swapchain_framebuffer()->handle();
+        info.renderArea.extent.width  = m_width;
+        info.renderArea.extent.height = m_height;
+        info.clearValueCount          = 2;
+        info.pClearValues             = &clear_values[0];
 
-        // Bind vertex array.
-        m_mesh->mesh_vertex_array()->bind();
+        vkCmdBeginRenderPass(cmd_buf->handle(), &info, VK_SUBPASS_CONTENTS_INLINE);
 
-        // Set active texture unit uniform
-        m_program->set_uniform("s_Diffuse", 0);
+        vkCmdBindPipeline(cmd_buf->handle(), VK_PIPELINE_BIND_POINT_GRAPHICS, m_pso->handle());
+
+        VkViewport vp;
+
+        vp.x        = 0.0f;
+        vp.y        = (float)m_height;
+        vp.width    = (float)m_width;
+        vp.height   = -(float)m_height;
+        vp.minDepth = 0.0f;
+        vp.maxDepth = 1.0f;
+
+        vkCmdSetViewport(cmd_buf->handle(), 0, 1, &vp);
+
+        VkRect2D scissor_rect;
+
+        scissor_rect.extent.width  = m_width;
+        scissor_rect.extent.height = m_height;
+        scissor_rect.offset.x      = 0;
+        scissor_rect.offset.y      = 0;
+
+        vkCmdSetScissor(cmd_buf->handle(), 0, 1, &scissor_rect);
+
+        const uint32_t dynamic_offset = m_ubo_size * m_vk_backend->current_frame_idx();
+
+        vkCmdBindDescriptorSets(cmd_buf->handle(), VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline_layout->handle(), 0, 1, &m_per_frame_ds->handle(), 1, &dynamic_offset);
+
+        VkDeviceSize offset = 0;
+        vkCmdBindVertexBuffers(cmd_buf->handle(), 0, 1, &m_mesh->vertex_buffer()->handle(), &offset);
+        vkCmdBindIndexBuffer(cmd_buf->handle(), m_mesh->index_buffer()->handle(), 0, VK_INDEX_TYPE_UINT32);
 
         const auto& submeshes = m_mesh->sub_meshes();
 
@@ -244,21 +391,22 @@ private:
             auto& submesh = submeshes[i];
             auto& mat     = m_mesh->material(submesh.mat_idx);
 
-            // Bind texture.
-            if (mat->albedo_texture())
-                mat->albedo_texture()->bind(0);
+            vkCmdBindDescriptorSets(cmd_buf->handle(), VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline_layout->handle(), 1, 1, &mat->descriptor_set()->handle(), 0, nullptr);
 
             // Issue draw call.
-            glDrawElementsBaseVertex(
-                GL_TRIANGLES, submesh.index_count, GL_UNSIGNED_INT, (void*)(sizeof(unsigned int) * submesh.base_index), submesh.base_vertex);
+            vkCmdDrawIndexed(cmd_buf->handle(), submesh.index_count, 1, submesh.base_index, submesh.base_vertex, 0);
         }
+
+        render_gui(cmd_buf);
+
+        vkCmdEndRenderPass(cmd_buf->handle());
     }
 
     // -----------------------------------------------------------------------------------------------------------------------------------
 
-    void update_uniforms()
+    void update_uniforms(dw::vk::CommandBuffer::Ptr cmd_buf)
     {
-        DW_SCOPED_SAMPLE("update_uniforms");
+        DW_SCOPED_SAMPLE("update_uniforms", cmd_buf);
 
         m_transforms.model      = glm::mat4(1.0f);
         m_transforms.model      = glm::translate(m_transforms.model, glm::vec3(0.0f, -20.0f, 0.0f));
@@ -267,19 +415,20 @@ private:
         m_transforms.view       = m_main_camera->m_view;
         m_transforms.projection = m_main_camera->m_projection;
 
-        void* ptr = m_ubo->map(GL_WRITE_ONLY);
-        memcpy(ptr, &m_transforms, sizeof(Transforms));
-        m_ubo->unmap();
+        uint8_t* ptr = (uint8_t*)m_ubo->mapped_ptr();
+        memcpy(ptr + m_ubo_size * m_vk_backend->current_frame_idx(), &m_transforms, sizeof(Transforms));
     }
 
     // -----------------------------------------------------------------------------------------------------------------------------------
 
 private:
     // GPU resources.
-    dw::gl::Shader::Ptr  m_vs;
-    dw::gl::Shader::Ptr  m_fs;
-    dw::gl::Program::Ptr m_program;
-    dw::gl::Buffer::Ptr  m_ubo;
+    size_t                           m_ubo_size;
+    dw::vk::GraphicsPipeline::Ptr    m_pso;
+    dw::vk::PipelineLayout::Ptr      m_pipeline_layout;
+    dw::vk::DescriptorSetLayout::Ptr m_per_frame_ds_layout;
+    dw::vk::DescriptorSet::Ptr       m_per_frame_ds;
+    dw::vk::Buffer::Ptr              m_ubo;
 
     // Camera.
     std::unique_ptr<dw::Camera> m_main_camera;
